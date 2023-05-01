1753acb1b1017416a292d9d8eedf4bf06714028c
Lenovo <yscheema5@gmail.com>
2023-05-02 00:22:34 +0530

    fuck it we ball

--- main.py
+++ main.py
@@ -1,101 +1,81 @@
+#fuck it we ball
 import pygame
 import random
 
-#initial variables
-GRID_WIDTH = 49
-GRID_HEIGHT = 49
-CELL_SIZE = 10
-GRID_SPACING = 6
-
-#screen size
-WINDOW_WIDTH = GRID_WIDTH * CELL_SIZE
-WINDOW_HEIGHT = GRID_HEIGHT * CELL_SIZE
-
-#defining colours here to reduce mess later
-BG_COLOR = (0, 0, 0)
-VEHICLE_COLOR = (255, 0, 0)
-VEHICLE_COLOR_2 = (0, 255, 0)
-
+#initializations
 pygame.init()
+window = pygame.display.set_mode((900,900))
+clock = pygame.time.Clock()
+running = True
 
-window = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
-pygame.display.set_caption("Vehicle Simulation")
-
-
-#this needs some work
+#this the weird squares that move
 class Vehicle:
-    def __init__(self, x, y, color, direction = "up"):
-        self.x = x
-        self.y = y
-        self.color = color
-        self.direction = direction
-        self.rect = pygame.Rect(self.x * CELL_SIZE, self.y * CELL_SIZE, CELL_SIZE, CELL_SIZE)
+    def __init__ (self, posX, posY):
+        self.x = posX
+        self.y = posY
+        self.direction = [0,1]
+        self.size = 20
+        self.rect = pygame.Rect(self.x, self.y, self.size, self.size)
 
-    def move(self): #iske paas direction hai, tere pass nahi
-        if self.direction == "up":
-            self.y -= 1
-        elif self.direction == "down":
-            self.y += 1
-        elif self.direction == "left":
-            self.x -= 1
-        elif self.direction == "right":
-            self.x += 1
+    def move(self):
+        self.x += self.direction[0]
+        self.y += self.direction[1]
+        self.rect.update(self.x, self.y, self.size, self.size)
 
-    def draw(self):
-        self.rect = pygame.Rect(self.x * CELL_SIZE, self.y * CELL_SIZE, CELL_SIZE, CELL_SIZE)
-        pygame.draw.rect(window, self.color, self.rect)
+    def display(self):
+        #self.rect.update(self.x, self.y, self.size, self.size)
+        pygame.draw.rect(window, "red", self.rect)
 
+coordsList = [20,140,260,380,500,620,740,860]
+def drawGrid():
+    for i in coordsList:
+        pygame.draw.line(window, "black", (i+10,coordsList[0]+10), (i+10,coordsList[-1]+10))
+        pygame.draw.line(window, "black", (coordsList[0]+10,i+10), (coordsList[-1]+10,i+10))
 
-#currently randomly generating vehicles to check if simulation works, will be using proper algorithm later
+vehicleList = []
 def createVehicle():
-    if (random.randint(0,1)):
-        x = random.randint(0, (GRID_WIDTH - 1)//GRID_SPACING)*GRID_SPACING #dividsion & multiplcation to emulate gaps
-        y = random.choice([0, GRID_HEIGHT - 1])
+    vehicleList.append(Vehicle(random.choice(coordsList),random.choice(coordsList)))
 
-        color = VEHICLE_COLOR
-        direction = random.choice(["up", "down"]) #jada dimag nahi lagaya, marji se kisi bhi direction me chale jayega
-    else:
-        x = random.choice([0, GRID_WIDTH - 1])
-        y = random.randint(0, (GRID_HEIGHT - 1)//GRID_SPACING)*GRID_SPACING #dividsion & multiplcation to emulate gaps
-
-        color = VEHICLE_COLOR_2
-        direction = random.choice(["right","left"]) #jada dimag nahi lagaya, marji se kisi bhi direction me chale jayega
-    vehicle = Vehicle(x, y, color, direction)
-    vehicles.append(vehicle)
-
-
-vehicles = []
-for i in range(20):
+for i in range(10):
     createVehicle()
     
 
-running = True
-clock = pygame.time.Clock()
 
+#main loop
 while running:
-
-    clock.tick(30)
-    
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
+    window.fill("white")
+    drawGrid()
+
+    for vehicle in vehicleList:
+        vehicle.display()
+
     #collision detection
-    for i in range(len(vehicles)):
-        for j in range(i + 1, len(vehicles)):
-            if vehicles[i].rect.colliderect(vehicles[j].rect):
+    for i in range(len(vehicleList)):
+        for j in range(i + 1, len(vehicleList)):
+            if vehicleList[i].rect.colliderect(vehicleList[j].rect):
                 print("Collision between vehicle", i, "and vehicle", j)
 
-    for vehicle in vehicles:
+    for vehicle in vehicleList:
         vehicle.move()
-        if vehicle.x < 0 or vehicle.x >= GRID_WIDTH or vehicle.y < 0 or vehicle.y >= GRID_HEIGHT:
-            #removing vehiles if they go off screen, then creating new one to replace it
-            vehicles.remove(vehicle)
+
+        #if vehicle on intersection
+        if vehicle.x in coordsList and vehicle.y in coordsList:
+            if vehicle.direction[0]:
+                vehicle.direction = [0,random.choice([-1,1])]
+            else:
+                vehicle.direction = [random.choice([-1,1]),0]
+
+        #if vehicle out of grid
+        if vehicle.x < coordsList[0] or vehicle.x > coordsList[-1] or vehicle.y < coordsList[0] or vehicle.y > coordsList[-1]:
+            vehicleList.remove(vehicle)
             createVehicle()
-            
-    window.fill(BG_COLOR)
 
-    for vehicle in vehicles:
-        vehicle.draw()
 
-    pygame.display.update()
+    pygame.display.flip()
+    clock.tick(60)
+
+pygame.quit()
